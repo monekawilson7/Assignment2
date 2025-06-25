54ed1f0c7e8e30c84aff8bc051275965d478fe4c
moneka <monekawilson16@gmail.com>
2025-06-25 18:11:07 +0300

    Q1

--- Assignment2/Program.cs
+++ Assignment2/Program.cs
@@ -6,7 +6,16 @@ namespace Assignment2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            #region Q1
+            int number;
+            Console.WriteLine("Enter a number: ");
+            number = int.Parse(Console.ReadLine());
+            if (number % 3 == 0 && number % 4 == 0)
+                Console.WriteLine("yes");
+            else
+                Console.WriteLine("no");
+
+            #endregion
         }
     }
 }
