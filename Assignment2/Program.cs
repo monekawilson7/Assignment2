98d2e7cc144157c1f181fd256aaca8047ec4dce2
moneka <monekawilson16@gmail.com>
2025-06-25 21:20:45 +0300

    Q6

--- Assignment2/Program.cs
+++ Assignment2/Program.cs
@@ -6,62 +6,107 @@ namespace Assignment2
     {
         static void Main(string[] args)
         {
-           
-            #region Q1
+            
+             #region Q1
+             int number;
+             Console.WriteLine("Enter a number: ");
+             number = int.Parse(Console.ReadLine());
+             if (number % 3 == 0 && number % 4 == 0)
+                 Console.WriteLine("yes");
+             else
+                 Console.WriteLine("no");
+             #endregion
+             #region Q2
+             int number;
+             Console.WriteLine("Enter a number: ");
+             number = int.Parse(Console.ReadLine());
+             if (number >= 0)
+                 Console.WriteLine("positive");
+             else
+                 Console.WriteLine("negative");
+             #endregion
+             #region Q3
+             int num1;
+             int num2;
+             int num3;
+             int max;
+             int min;
+             Console.WriteLine("Enter 3 numbers: ");
+             string[] input = Console.ReadLine().Split(new[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries); 
+             num1 = int.Parse(input[0]);
+             num2 = int.Parse(input[1]);
+             num3 = int.Parse(input[2]);
+             if (num1 >= num2 && num1 >= num3)
+             {
+                 max = num1;
+                 if(num2>=num3)
+                     min = num3;
+                 else 
+                     min = num2; 
+             }
+             else if (num2>=num1 && num2>=num3)
+             {
+                 max = num2;
+                 if (num1 >= num3)
+                     min = num3;
+                 else
+                     min = num1;
+             }
+             else
+             {
+                 max = num3;
+                 if (num1 >= num2)
+                     min = num2;
+                 else
+                     min = num1;
+             }
+             Console.WriteLine($"max element = {max}");
+             Console.WriteLine($"min element = {min}");
+             #endregion
+            #region Q4
             int number;
             Console.WriteLine("Enter a number: ");
             number = int.Parse(Console.ReadLine());
-            if (number % 3 == 0 && number % 4 == 0)
-                Console.WriteLine("yes");
+            if (number % 2 == 0)
+                Console.WriteLine("Even");
             else
-                Console.WriteLine("no");
+                Console.WriteLine("Odd");
             #endregion
-            #region Q2
+            #region Q5
+            char character;
+            Console.WriteLine("Enter a character: ");
+            character=Console.ReadKey().KeyChar;
+            Console.WriteLine();
+            switch (character)
+            { 
+             case 'a':
+                case 'e':
+                case 'i':
+                case 'o':
+                case 'u':
+                case 'A':
+                case 'E':
+                case 'I':
+                case 'O':
+                case 'U':
+                    Console.WriteLine("Vowel");
+                    break;
+                    default:
+                    Console.WriteLine("Consonant");
+                    break ;
+            }
+            #endregion
+            #region Q6
             int number;
             Console.WriteLine("Enter a number: ");
             number = int.Parse(Console.ReadLine());
-            if (number >= 0)
-                Console.WriteLine("positive");
-            else
-                Console.WriteLine("negative");
-            #endregion
-            #region Q3
-            int num1;
-            int num2;
-            int num3;
-            int max;
-            int min;
-            Console.WriteLine("Enter 3 numbers: ");
-            string[] input = Console.ReadLine().Split(new[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries); 
-            num1 = int.Parse(input[0]);
-            num2 = int.Parse(input[1]);
-            num3 = int.Parse(input[2]);
-            if (num1 >= num2 && num1 >= num3)
+            for (int i = 1; i <= number; i++)
             {
-                max = num1;
-                if(num2>=num3)
-                    min = num3;
-                else 
-                    min = num2; 
-            }
-            else if (num2>=num1 && num2>=num3)
-            {
-                max = num2;
-                if (num1 >= num3)
-                    min = num3;
+                if (i < number)
+                    Console.Write(i + ", ");
                 else
-                    min = num1;
+                    Console.Write(i);
             }
-            else
-            {
-                max = num3;
-                if (num1 >= num2)
-                    min = num2;
-                else
-                    min = num1;
-            }
-            Console.WriteLine($"max element = {max}");
-            Console.WriteLine($"min element = {min}");
             #endregion
         }
     }
