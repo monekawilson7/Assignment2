3417a402b6d6b8c0cd5b16c8820b7d267ea057c3
moneka <monekawilson16@gmail.com>
2025-06-25 20:29:27 +0300

    Q3

--- Assignment2/Program.cs
+++ Assignment2/Program.cs
@@ -6,6 +6,7 @@ namespace Assignment2
     {
         static void Main(string[] args)
         {
+           
             #region Q1
             int number;
             Console.WriteLine("Enter a number: ");
@@ -23,6 +24,44 @@ namespace Assignment2
                 Console.WriteLine("positive");
             else
                 Console.WriteLine("negative");
+            #endregion
+            #region Q3
+            int num1;
+            int num2;
+            int num3;
+            int max;
+            int min;
+            Console.WriteLine("Enter 3 numbers: ");
+            string[] input = Console.ReadLine().Split(new[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries); 
+            num1 = int.Parse(input[0]);
+            num2 = int.Parse(input[1]);
+            num3 = int.Parse(input[2]);
+            if (num1 >= num2 && num1 >= num3)
+            {
+                max = num1;
+                if(num2>=num3)
+                    min = num3;
+                else 
+                    min = num2; 
+            }
+            else if (num2>=num1 && num2>=num3)
+            {
+                max = num2;
+                if (num1 >= num3)
+                    min = num3;
+                else
+                    min = num1;
+            }
+            else
+            {
+                max = num3;
+                if (num1 >= num2)
+                    min = num2;
+                else
+                    min = num1;
+            }
+            Console.WriteLine($"max element = {max}");
+            Console.WriteLine($"min element = {min}");
             #endregion
         }
     }
